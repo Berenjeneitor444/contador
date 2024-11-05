b05a4c5566735e30d90ba3cfc3457c85aeb195ec
Berenjeneitor444 <dciriano2@gmail.com>
2024-11-05 18:17:41 +0100

    contador finalizado

--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.contador;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView numero;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        numero = findViewById(R.id.textView);
+        findViewById(R.id.count).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numero.setText(String.valueOf(Integer.parseInt(numero.getText().toString()) + 1));
+            }
+        });
+        findViewById(R.id.toast).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(MainActivity.this, "You have counted " + numero.getText() + " times", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
     }
 }
